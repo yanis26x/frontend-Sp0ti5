41a93c9812cd3859ebf3a417bf1b24245f7eadcd
“ManasseTegGbegnohou” <mtgbegnohou@gmail.com>
2025-12-12 20:06:07 -0500

    update vite server so it is not bond to localhost

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.VITE_PORT || '5173'),
+    strictPort: true,
+  },
 })
